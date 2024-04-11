113cedfab84a01ad1905901c1585ba014cbbb09e
James Mapepa <jameszmapepa@gmail.com>
2024-04-11 10:35:14 +0200

    Add Stream debugging

--- src/main/java/io/jameszmapepa/BreakpointsDialog.java
+++ src/main/java/io/jameszmapepa/BreakpointsDialog.java
@@ -1,10 +1,9 @@
 package io.jameszmapepa;
 
 
+import java.util.stream.Stream;
+
 public class BreakpointsDialog
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+
 }
--- src/main/java/io/jameszmapepa/StreamDebugging.java
+++ src/main/java/io/jameszmapepa/StreamDebugging.java
@@ -1,13 +1,31 @@
 package io.jameszmapepa;
 
-/**
- * Hello world!
- *
- */
+import java.util.Arrays;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+
 public class StreamDebugging
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args) {
+        Integer[] numbers = {1,18,2,6,3,4,5,6,15,6,7,8,9,10,11,3,6,12,13,14,15,16,1,16,7,17,18,19};
+
+
+       Integer[] oddPrimeNumbersGreaterThanFive= Stream.of(numbers)
+               .distinct().sorted().filter(i -> i % 2 != 0).filter(i->i>4).filter(StreamDebugging::isPrime)
+               .toArray(Integer[]::new);
+
+        System.out.println("Even numbers greater than four :"+ Arrays.toString(oddPrimeNumbersGreaterThanFive));
     }
+
+    public static boolean isPrime(int number) {
+    if (number <= 1) {
+        return false;
+    }
+    return IntStream.rangeClosed(2, (int) Math.sqrt(number))
+            .allMatch(n -> number % n != 0);
+}
+
+
 }
